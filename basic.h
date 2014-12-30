3b80c095f0465ef6f0339b398151fed41eac4df1
unknown <oleksiiav@ptua-ls0194.EE.playtech.corp>
2014-12-31 00:23:23 +0200

    More list operations

--- basic.h
+++ basic.h
@@ -6,22 +6,21 @@
 // Basic element
 template <int E> struct Element : std::integral_constant<int, E> {};
 
-// Empty type declaration
-struct Empty {};
-
 // List declaration
 template <int... Is> struct List {};
 
 // Head
 // Example:
-//		Head<List<1,2,3>> === Element<1>
+//    Head<List<1,2,3>> === Element<1>
 // ===========================================================================
 template <class T> struct Head_;
 
 template <int... Is> struct Head_<List<Is...>> {
-  template <int H, int... Tl> using HeadImpl = Element<H>;
+  // template <int H, int... Tl> using HeadImpl = Element<H>;
 
-  using type = HeadImpl<Is...>;
+  template <int H, int... Tl> struct HeadImpl { using type = Element<H>; };
+
+  using type = typename HeadImpl<Is...>::type;
 };
 
 template <> struct Head_<List<>> {};
@@ -31,7 +30,7 @@ template <class T> using Head = typename Head_<T>::type;
 
 // Tail
 // Example:
-//		Tail<List<1,2,3>> === List<2,3>
+//    Tail<List<1,2,3>> === List<2,3>
 // ===========================================================================
 template <class T> struct Tail_;
 
@@ -46,30 +45,32 @@ template <> struct Tail_<List<>> {};
 template <class T> using Tail = typename Tail_<T>::type;
 // ===========================================================================
 
-// Replicate list
-// Makes a List by replicating input list N times
+// Length of a List
 // Example:
-// 		ReplicateList<2, List<1,2,3>> === List<1,2,3,1,2,3>
+//    Length<List<1,2,3>>::value === 3
 // ===========================================================================
-template <int N, class T> struct ReplicateList;
+template <class T> struct Length;
 
-template <int N, int... Is> struct ReplicateList<N, List<Is...>> {
-  template <int Num, int... Ms>
-  struct ReplicateImpl : ReplicateImpl<Num - 1, Is..., Ms...> {};
+template <int... Is>
+struct Length<List<Is...>> : std::integral_constant<int, sizeof...(Is)> {};
+// ===========================================================================
 
-  template <int... Bs> struct ReplicateImpl<1, Bs...> {
-    using type = List<Bs...>;
-  };
+// Test whether List is Empty
+// Example:
+//    IsEmpty<List<1,2,3>>::value === false
+//    IsEmpty<List<>>::value === true
+// ===========================================================================
+template <class T> struct IsEmpty;
 
-  template <int... Bs> struct ReplicateImpl<0, Bs...> { using type = Empty; };
+template <int... Is>
+struct IsEmpty<List<Is...>> : std::integral_constant<bool, false> {};
 
-  using type = typename ReplicateImpl<N, Is...>::type;
-};
+template <> struct IsEmpty<List<>> : std::integral_constant<bool, true> {};
 // ===========================================================================
 
 // Makes a List from 0 up to N-1
 // Example:
-//		MakeSeq<6> === List<0,1,2,3,4,5>
+//    MakeSeq<6> === List<0,1,2,3,4,5>
 // ===========================================================================
 template <int N, int... Is>
 struct MakeSeqImpl : MakeSeqImpl<N - 1, N - 1, Is...> {};
@@ -82,22 +83,22 @@ using MakeSeq = typename MakeSeqImpl<N, Is...>::type;
 
 // Makes a list by replicating single value "Num" "N" times
 // Example:
-//		ReplicateValue<3, 42> === List<42, 42, 42>
+//    ReplicateValue<3, 42> === List<42, 42, 42>
 // ===========================================================================
 template <int N, int Num, int... Is>
-struct ReplicateValueImpl : ReplicateValueImpl<N - 1, Num, Num, Is...> {};
+struct ReplicateValue_ : ReplicateValue_<N - 1, Num, Num, Is...> {};
 
-template <int... Is> struct ReplicateValueImpl<1, Is...> {
+template <int... Is> struct ReplicateValue_<1, Is...> {
   using type = List<Is...>;
 };
 
 template <int N, int Num, int... Is>
-using ReplicateValue = typename ReplicateValueImpl<N, Num, Is...>::type;
+using ReplicateValue = typename ReplicateValue_<N, Num, Is...>::type;
 // ===========================================================================
 
 // Concatanate two Lists
 // Example:
-//		Concat<List<1,2,3>, List<4,5,6>> === List<1, 2, 3, 4, 5, 6 >
+//    Concat<List<1,2,3>, List<4,5,6>> === List<1, 2, 3, 4, 5, 6 >
 // ===========================================================================
 template <class T, class U> struct Concat_;
 
@@ -108,9 +109,65 @@ template <int... Is, int... Js> struct Concat_<List<Is...>, List<Js...>> {
 template <class T, class U> using Concat = typename Concat_<T, U>::type;
 // ===========================================================================
 
+// Take first N values from a List producing a new List
+// Example:
+//    Take<2, List<1,2,3>> === List<1, 2>
+// ===========================================================================
+template <int N, class T> struct Take_;
+
+template <int N, int... Is> struct Take_<N, List<Is...>> {
+  template <int M, class L> struct TakeImpl;
+
+  template <int M, int... Zs> struct TakeImpl<M, List<Zs...>> {
+    using tail = typename TakeImpl<M - 1, Tail<List<Zs...>>>::type;
+    using type = Concat<List<Head<List<Zs...>>::value>, tail>;
+  };
+
+  template <int... Zs> struct TakeImpl<0, List<Zs...>> { using type = List<>; };
+
+  using type = typename TakeImpl<N, List<Is...>>::type;
+};
+
+template <int N, class T> using Take = typename Take_<N, T>::type;
+// ===========================================================================
+
+// Drop first N values from a List producing a new List
+// Example:
+//    Drop<2, List<1,2,3,4>> === List<3, 4>
+// ===========================================================================
+template <int N, class T> struct Drop_;
+
+template <int N, int... Is>
+struct Drop_<N, List<Is...>> : Drop_<N - 1, Tail<List<Is...>>> {};
+
+template <int... Is> struct Drop_<0, List<Is...>> { using type = List<Is...>; };
+
+template <int N, class T> using Drop = typename Drop_<N, T>::type;
+// ===========================================================================
+
+// Replicate list
+// Makes a List by replicating input list N times
+// Example:
+//    ReplicateList<2, List<1,2,3>> === List<1,2,3,1,2,3>
+// ===========================================================================
+template <int N, class T> struct ReplicateList_;
+
+template <int N, int... Is> struct ReplicateList_<N, List<Is...>> {
+  using tail = typename ReplicateList_<N - 1, List<Is...>>::type;
+  using type = typename Concat<List<Is...>, tail>;
+};
+
+template <int... Is> struct ReplicateList_<1, List<Is...>> {
+  using type = List<Is...>;
+};
+
+template <int N, class T>
+using ReplicateList = typename ReplicateList_<N, T>::type;
+// ===========================================================================
+
 // Zips two Lists
 // Example:
-//		Zip<List<1,2,3>, List<4,5,6>> === List<1, 4, 2, 5, 3, 6 >
+//    Zip<List<1,2,3>, List<4,5,6>> === List<1, 4, 2, 5, 3, 6 >
 // ===========================================================================
 template <class T, class U> struct Zip_;
 
@@ -123,5 +180,79 @@ template <int... Is, int... Js> struct Zip_<List<Is...>, List<Js...>> {
 template <> struct Zip_<List<>, List<>> { using type = List<>; };
 
 template <class T, class U> using Zip = typename Zip_<T, U>::type;
+
+// 1-ary functions on Element
+// Example:
+//    Id< Element <2> >::value === 2
+//    Negate< Element <2> >::value === -2
+//    Abs< Element <-2> >::value === 2
+// ===========================================================================
+template <class T> struct Id { using type = T; };
+
+template <class T> struct Negate : std::integral_constant<int, -T::value> {};
+
+template <class T>
+struct Abs
+    : std::conditional<(T::value > 0), std::integral_constant<int, T::value>,
+                       std::integral_constant<int, -T::value>>::type {};
+
+// Composition of 1-ary functions on Element
+// Example:
+//    Compose<Negate, Abs>::Result< Element <-2> >::value === -2
+// ===========================================================================
+template <template <class> class F, template <class> class G> struct Compose {
+  template <class X>
+  struct Result : std::integral_constant<int, F<Element<G<X>::value>>::value> {
+  };
+};
+
+// 2-ary functions on Element
+// Example:
+//    Add< Element <2>, Element<3> >::value === 5
+//    Substract< Element <2>, Element<3> >::value === -1
+// ===========================================================================
+template <class T, class V>
+struct Add : std::integral_constant<int, T::value + V::value> {};
+
+template <class T, class V>
+struct Substract : std::integral_constant<int, T::value - V::value> {};
+
+// List folding operation (left)
+// Takes List, Initial value and Function and produces a single value
+// Example:
+//    Foldl< List<1,2,3>, 0, Add>::value === 6
+//    Foldl< List<1,2,3>, 0, Substract>::value === -6
+// ===========================================================================
+template <class L, int Init, template <class, class> class F> struct Foldl;
+
+template <int... Is, int Init, template <class, class> class F>
+struct Foldl<List<Is...>, Init, F> {
+  static const int value = F<
+      Element<0>,
+      Element<Foldl<Tail<List<Is...>>,
+                    Element<F<Element<Init>, Head<List<Is...>>>::value>::value,
+                    F>::value>>::value;
+};
+
+template <int Init, template <class, class> class F>
+struct Foldl<List<>, Init, F> : std::integral_constant<int, Init> {};
+// ===========================================================================
+
+// Map, apply function to each element of a List producing new List
+// Example:
+//    Map< Abs, List<1, -2, 3> > === List <1, 2, 3>
+//    Map< Negate, List<-1, 2, -3> > === List <1, -2, 3>
+// ===========================================================================
+template <template <class> class F, class T> struct Map;
+
+template <template <class> class F, int... Is> struct Map<F, List<Is...>> {
+  using tail = typename Map<F, Tail<List<Is...>>>::type;
+  using type = Concat<List<F<Head<List<Is...>>>::value>, tail>;
+};
+
+template <template <class> class F> struct Map<F, List<>> {
+  using type = List<>;
+};
+// ===========================================================================
 
 #endif __BASIC_H__
